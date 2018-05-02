3c3e8aaa8b332bb8e51fa6c0796df4dc4f2958f0
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2018-05-02 09:04:34 +0200

    MVC project added

---
+++ MyWebApp/MyWebAppMvc/Models/Car.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyWebAppMvc.Models
+{
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Brand { get; set; }
+
+        public DateTime CreationDate { get; set; }
+    }
+}
