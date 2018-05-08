1fafa37131b778c58970c4d7280ee4d14b7b8427
Mathias Kirkegaard Nielsen <matn@bluefragments.com>
2018-05-08 16:07:07 +0200

    more test projects

---
+++ MyWebApp/MyToDoAppMathias_Xamarin_Forms/MyToDoAppMathias/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyToDoAppMathias
+{
+	public static class Constants
+	{
+		// Replace strings with your Azure Mobile App endpoint.
+		public static string ApplicationURL = @"https://mytodoappmathias.azurewebsites.net";
+	}
+}
+
